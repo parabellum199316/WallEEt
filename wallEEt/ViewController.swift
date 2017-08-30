3f8e5f1c393078aeb18cdd942aa42d359bc5ed41
parabellum199316 <parabellum199316@gmail.com>
2017-08-30 17:22:23 +0300

    Initial Commit

---
+++ wallEEt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wallEEt
+//
+//  Created by ParaBellum on 8/30/17.
+//  Copyright © 2017 ParaBellum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
